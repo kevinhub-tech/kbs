f421b463afc476aefdd0875435f7e406c8753550
Win Khant Paing <winkhantpaing32@gmail.com>
2024-07-24 20:27:19 +0700

    rearranged web route

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('vendor')->group(function () {
+    Route::get('/login', function () {
+        return view('login');
+    })->name('vendor.login');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/login', function () {
+        return view('login');
+    })->name('admin.login');
+});
+
+Route::prefix('user')->group(function () {
+    Route::get('/login', function () {
+        return view('login');
+    })->name('user.login');
+    Route::get('/home', function () {
+        return view('users.home');
+    });
+});
+
 Route::get('/', function () {
     return view('main');
-});
-
-Route::get('/user/home', function () {
-    return view('users.home');
-});
-
-Route::get('/user/login', function () {
-    return view('login');
 });
