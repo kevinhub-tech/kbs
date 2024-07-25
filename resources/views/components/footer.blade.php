b1966897724bcf7facd2d8c8fb9c1f8635142bfc
Win Khant Paing <winkhantpaing32@gmail.com>
2024-07-25 20:58:29 +0700

    fixed nav bar and completed footer partially

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,39 @@
 <footer>
-    <h2>Footer</h2>
+    <div class="d-flex align-items-center justify-content-around">
+        <div class="logo">
+            <a class="nav-logo d-flex align-items-center ms-md-5 ms-0 mb-3" href='#'>
+                <i class="fa-solid fa-lightbulb"></i>
+                <h2 class="m-0 ms-2">
+                    KBS
+                </h2>
+            </a>
+            <p>
+                Welcome to KBS: Kevin Book Store where every imaginable books is at your avail.
+                Only Quality books by local vendors and partners.
+            </p>
+        </div>
+        <div class="user-links">
+            User
+            <ul>
+                <li><a href="">Track Order </a></li>
+                <li><a href="">How to Buy </a></li>
+                <li><a href="">Terms and Conditions </a></li>
+                <li><a href="">FAQ </a></li>
+            </ul>
+        </div>
+        <div class="vendor-links">
+            Vendors
+            <ul>
+                <li><a href="">Become a Partner </a></li>
+                <li><a href="">Terms and Conditions </a></li>
+                <li><a href="">FAQ </a></li>
+            </ul>
+        </div>
+        <div class="social-links">
+            Follow Our Socials
+            <div class="social-links">
+
+            </div>
+        </div>
+    </div>
 </footer>
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,15 +1,16 @@
 <nav>
     {{-- This will be universally used --}}
-    <div class="main-nav-container">
+    <div class="main-nav-container d-flex flex-column flex-lg-row align-items-center justify-content-between">
         {{-- Logo --}}
-        <a class="nav-logo d-flex align-items-center" href='#'>
+        <a class="nav-logo d-flex align-items-center ms-md-5 ms-0 mb-3" href='#'>
             <i class="fa-regular fa-lightbulb"></i>
             <h2 class="m-0">
                 KBS
             </h2>
         </a>
         {{-- Search bar if user is customer --}}
-        <div class="search-bar">
+
+        <div class="search-bar mb-3">
             <select name="" id="">
                 <option value="books">Books</option>
                 <option value="author">Authors</option>
@@ -20,15 +21,20 @@
         </div>
 
 
+
         {{-- These links will change based on user role --}}
-        <ul>
-            <li><a href="#">Login</a></li>
-            <li><a href="#">Register</a></li>
-        </ul>
-    </div>
 
-    {{-- If user is customer, include this sub menu with genre of books --}}
-    <div class="sub-nav-container pt-1">
+        @if (Request::url() === route('user.login') || Request::url() === route('user.register'))
+            <ul>
+                <li><a href="{{ route('user.login') }}">Login</a></li>
+                <li><a href="{{ route('user.register') }}">Register</a></li>
+            </ul>
+        @else
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-heart"></i></a></li>
+                <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
+            </ul>
+        @endif
     </div>
-
 </nav>
