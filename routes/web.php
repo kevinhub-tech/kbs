5b96117e3eacbbd682b1e47976faa91309133a68
Win Khant Paing <winkhantpaing32@gmail.com>
2024-07-31 23:02:36 +0700

    proper routing provided

--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,22 @@ Route::controller(VendorController::class)->prefix('vendor')->group(function () 
     Route::get('/login', function () {
         return view('login');
     })->name('vendor.login');
+    Route::post('/signin', 'signin')->name('vendor.manuallogin');
 });
 
 Route::controller(AdminController::class)->prefix('admin')->group(function () {
     Route::get('/login', function () {
         return view('login');
     })->name('admin.login');
+    Route::post('/signin', 'signin')->name('admin.manuallogin');
 });
 
 Route::controller(UserController::class)->prefix('user')->group(function () {
-    Route::get('/login', function () {
-        return view('login');
-    })->name('user.login');
-    Route::get('/register', function () {
-        return view('users.register');
-    })->name('user.register');
-    Route::get('/', function () {
-        return view('users.home');
-    });
+    Route::get('/login', 'login')->name('user.login');
+    Route::get('/register', 'register')->name('user.register');
+    Route::post('/signin', 'signin')->name('user.manuallogin');
+    Route::post('/signup', 'signup')->name('user.manualregister');
+    Route::get('/', 'home')->name("user.home");
     Route::get('/auth/{social}/redirect', 'auth')->name('auth.login');
     Route::get('/auth/{social}/callback', 'authcallback')->name('auth.callback');
 });
