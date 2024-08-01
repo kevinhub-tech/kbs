8d76529eda772aa1f4d319af1befbd9e0500b380
Win Khant Paing <winkhantpaing32@gmail.com>
2024-08-01 21:02:39 +0700

    added middleware on necessary routes

--- routes/web.php
+++ routes/web.php
@@ -3,37 +3,44 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VendorController;
+use App\Http\Middleware\UserOnlyAccess;
+use App\Http\Middleware\VendorOnlyAccess;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 
 
 Route::controller(VendorController::class)->prefix('vendor')->group(function () {
-    Route::get('/', function () {
-        return view('vendors.home');
+    Route::middleware('vendor-only')->group(function () {
+        Route::get('/', 'home')->name('vendor.home');
     });
-    Route::get('/login', function () {
-        return view('login');
-    })->name('vendor.login');
+    Route::get('/login', 'login')->name('vendor.login');
     Route::post('/signin', 'signin')->name('vendor.manuallogin');
 });
 
 Route::controller(AdminController::class)->prefix('admin')->group(function () {
-    Route::get('/login', function () {
-        return view('login');
-    })->name('admin.login');
+    Route::middleware('admin-only')->group(function () {
+        Route::get('/', 'home')->name('admin.home');
+    });
+    Route::get('/login', 'login')->name('admin.login');
     Route::post('/signin', 'signin')->name('admin.manuallogin');
 });
 
 Route::controller(UserController::class)->prefix('user')->group(function () {
     Route::get('/login', 'login')->name('user.login');
     Route::get('/register', 'register')->name('user.register');
+    Route::middleware('user-only')->group(function () {
+        Route::get('/', 'home')->name("user.home");
+    });
     Route::post('/signin', 'signin')->name('user.manuallogin');
     Route::post('/signup', 'signup')->name('user.manualregister');
-    Route::get('/', 'home')->name("user.home");
+
     Route::get('/auth/{social}/redirect', 'auth')->name('auth.login');
     Route::get('/auth/{social}/callback', 'authcallback')->name('auth.callback');
 });
 
+Route::get('/accessDenied', function () {
+    return view('accessDenied');
+})->name('accessDeny');
 
 Route::get('/', function () {
     return view('main');
