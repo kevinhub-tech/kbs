b6a8631aa1a890c6244558bdc94682f4d4ce2a44
Win Khant Paing <winkhantpaing32@gmail.com>
2024-08-04 17:32:36 +0700

    return to previous page on accessDeined implemented

--- resources/views/accessDenied.blade.php
+++ resources/views/accessDenied.blade.php
@@ -3,7 +3,7 @@
     <div class="d-flex justify-content-center align-items-center h-100">
         <div class="denied-card">
             <h4>{{ session('accessDeny') }} ⚠️</h4>
-            <a href="{{ route('user.login') }}" class="return">Login</a>
+            <a href="javascript:history.back()" class="return">Go Back</a>
         </div>
     </div>
 @endsection
