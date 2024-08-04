241e8e38fd28dd6cb6ece4f635c4ebfb2fd0fcb7
Win Khant Paing <winkhantpaing32@gmail.com>
2024-08-04 17:31:48 +0700

    api routes defined

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::controller(VendorController::class)->prefix('vendor')->group(function () {
+    Route::middleware('vendor-only')->group(function () {
+        Route::get('/get', 'demoget');
+        Route::post('/post', 'demopost');
+    });
+});
+
+Route::controller(AdminController::class)->prefix('admin')->group(function () {
+    Route::middleware('admin-only')->group(function () {
+        Route::get('/get', 'demoget');
+        Route::post('/post', 'demopost');
+    });
+});
+
+Route::controller(UserController::class)->prefix('user')->group(function () {
+    Route::middleware('user-only')->group(function () {
+        Route::get('/get', 'demoget');
+        Route::post('/post', 'demopost');
+    });
+});
