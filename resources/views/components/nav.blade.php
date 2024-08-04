a0a403eaa32f529bb2090d8b6d30439b453a242d
Win Khant Paing <winkhantpaing32@gmail.com>
2024-08-04 17:32:54 +0700

    dynamic nav bar implemented

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -10,31 +10,71 @@
         </a>
         {{-- Search bar if user is customer --}}
 
-        <div class="search-bar mb-3">
-            <select name="" id="">
-                <option value="books">Books</option>
-                <option value="author">Authors</option>
-                <option value="genre">Genre</option>
-            </select>
-            <input type="text" name="search" id="search-bar" placeholder="Search">
-            <i class="fa-solid fa-magnifying-glass"></i>
-        </div>
-
-
-
-        {{-- These links will change based on user role --}}
-
-        @if (Request::url() === route('user.login') || Request::url() === route('user.register'))
-            <ul>
-                <li><a href="{{ route('user.login') }}">Login</a></li>
-                <li><a href="{{ route('user.register') }}">Register</a></li>
-            </ul>
-        @else
+        @if (session('userSignedIn') && session('userRole') === 'user')
+            <div class="search-bar mb-3">
+                <select name="" id="">
+                    <option value="books">Books</option>
+                    <option value="author">Authors</option>
+                    <option value="genre">Genre</option>
+                </select>
+                <input type="text" name="search" id="search-bar" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
             <ul>
                 <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
                 <li><a href="#"><i class="fa-solid fa-heart"></i></a></li>
                 <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
             </ul>
+        @endif
+
+        @if (session('userSignedIn') && session('userRole') === 'vendor')
+            <div class="search-bar mb-3">
+                <select name="" id="">
+                    <option value="books">Books</option>
+                    <option value="author">Authors</option>
+                    <option value="genre">Genre</option>
+                </select>
+                <input type="text" name="search" id="search-bar" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+            <ul>
+                <li><a href="#">Manage Orders</a></li>
+                <li><a href="#">Manage Books</a></li>
+                <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
+
+            </ul>
+        @endif
+
+        @if (session('userSignedIn') && session('userRole') === 'admin')
+            <div class="search-bar mb-3">
+                <select name="" id="">
+                    <option value="books">Books</option>
+                    <option value="author">Authors</option>
+                    <option value="genre">Genre</option>
+                </select>
+                <input type="text" name="search" id="search-bar" placeholder="Search">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+            <ul>
+                <li><a href="#">Manage Users</a></li>
+                <li><a href="#">Manage Vendors</a></li>
+                <li><a href="#"><i class="fa-solid fa-user"></i></a></li>
+            </ul>
+        @endif
+
+
+
+
+        {{-- These links will change based on user role --}}
+
+        @if (
+            (!session()->has('userSignedIn') && Request::url() === route('user.login')) ||
+                Request::url() === route('user.register'))
+            <ul>
+                <li><a href="{{ route('user.login') }}">Login</a></li>
+                <li><a href="{{ route('user.register') }}">Register</a></li>
+            </ul>
+        @else
         @endif
     </div>
 </nav>
