10f5e75a88f5ca0cc4cfd1b50ed8e5b491d9ebb8
Win Khant Paing <winkhantpaing32@gmail.com>
2024-08-06 20:36:44 +0700

    added toast notification + popup library and managed css

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -15,7 +15,10 @@
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+    {{-- custom css and js to use globally --}}
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <script type="text/javascript" src='{{ asset('js/main.js') }}'></script>
+
     {{-- Jquery CDN --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
@@ -23,6 +26,13 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+
+    {{-- toastify CDN --}}
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+
+    {{-- sweetalert2 CDN --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @stack('head')
 </head>
 
