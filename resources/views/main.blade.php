dc592abbcf70ee74e65f84c6a304a8771b55e843
Win Khant Paing <winkhantpaing32@gmail.com>
2024-08-13 22:36:11 +0700

    added multidropdown cdn

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -33,6 +33,12 @@
 
     {{-- sweetalert2 CDN --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    {{-- Multi-select dropdown CDN --}}
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@3.1.0/dist/css/multi-select-tag.css">
+    <script src="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@3.1.0/dist/js/multi-select-tag.js"></script>
+
     @stack('head')
 </head>
 
