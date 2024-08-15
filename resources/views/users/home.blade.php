947c1eae273803c4f6cd9950c85203aab75d5d67
Win Khant Paing <winkhantpaing32@gmail.com>
2024-08-15 23:46:36 +0700

    front-end logic for cart and favourite added

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -51,7 +51,7 @@
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                             aria-label="Close"></button>
                                                 </div>
-                                                <div class="modal-body">
+                                                <div class="modal-body" id="{{ $book->book_id }}">
                                                     <div class='d-flex justify-content-evenly align-items-center'>
                                                         <img src="{{ route('get-image', ['image' => $book->image]) }}"
                                                             alt="" class="book-image">
@@ -81,12 +81,13 @@
                                                 <div class="modal-footer">
                                                     <button type="button" class="btn btn-secondary"
                                                         data-bs-dismiss="modal">Close</button>
-                                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                                    <button type="button" class="btn kbs-btn"
+                                                        data-book-id='{{ $book->book_id }}' data-quantity='1'>Add</button>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <i class="fa-solid fa-heart"></i>
+                                    <i class="fa-solid fa-heart add-to-favourite" data-book-id="{{ $book->book_id }}"></i>
                                 </div>
                             </div>
                         </div>
@@ -133,6 +134,8 @@
                 let totalPriceElement = button.parentElement.nextElementSibling.children[0];
                 let totalPrice = parseFloat(button.parentElement.nextElementSibling.children[0]
                     .getAttribute('original-price'));
+                let addButton = button.parentElement.parentElement.parentElement.parentElement
+                    .nextElementSibling.children[1];
                 if (buttonClassName === 'add-quantity') {
                     let quantityElement = button.previousElementSibling;
                     let quantity = parseInt(quantityElement.innerHTML);
@@ -148,6 +151,7 @@
                     }
                     totalPrice *= quantity;
                     totalPriceElement.innerHTML = totalPrice;
+                    addButton.dataset.quantity = quantity;
                 } else {
                     let quantityElement = button.nextElementSibling;
                     let quantity = parseInt(quantityElement.innerHTML);
@@ -164,7 +168,139 @@
                     }
                     totalPrice *= quantity;
                     totalPriceElement.innerHTML = totalPrice;
+                    addButton.dataset.quantity = quantity;
                 }
+            })
+            $("div.modal-footer button.kbs-btn").on('click', function(e) {
+                // Get book_id and quantity
+                let bookId = $(this).data('book-id');
+                let quantity = $(this).data('quantity');
+
+                // Call ajax and once it is succesful, check cart-count and add that number there
+                $.ajax({
+                    url: "{{ route('user.addcart') }}",
+                    method: 'POST',
+                    headers: {
+                        Accept: "application/json",
+                        Authorization: "{{ session('userToken') }}"
+                    },
+                    data: {
+                        book_id: bookId,
+                        quantity: quantity
+                    },
+                    success: (res) => {
+                        if (res.status === 'success') {
+                            toast('success', res.message);
+                            $("small.cart-count").html(res.payload.cart_count);
+                            $("small.cart-count").css('display', 'flex');
+                            setTimeout(() => {
+                                $(`#${bookId}`).modal('hide');
+                            }, 1500);
+                        }
+                    },
+                    error: (jqXHR, exception) => {
+                        var errorMessage = "";
+
+                        if (jqXHR.status === 0) {
+                            errorMessage =
+                                "Not connect.\n Verify Network.";
+                        } else if (jqXHR.status == 404) {
+                            errorMessage =
+                                "Requested page not found. [404]";
+                        } else if (jqXHR.status == 409) {
+                            errorMessage = jqXHR.responseJSON.message;
+                        } else if (jqXHR.status == 500) {
+                            errorMessage =
+                                "Internal Server Error [500].";
+                        } else if (exception === "parsererror") {
+                            errorMessage =
+                                "Requested JSON parse failed.";
+                        } else if (exception === "timeout") {
+                            errorMessage = "Time out error.";
+                        } else if (exception === "abort") {
+                            errorMessage = "Ajax request aborted.";
+                        } else {
+                            let html = ''
+                            Object.values(jqXHR.responseJSON.errors).forEach((err) => {
+                                err.forEach((e) => {
+                                    html += `${e} <hr />`;
+                                });
+                            });
+                            Swal.fire({
+                                title: 'Error!',
+                                html: html,
+                                icon: 'error',
+                                animation: true,
+                                showConfirmButton: true,
+                            })
+                            return;
+                        }
+                        toast("error", errorMessage);
+                    }
+                })
+            })
+
+            $("i.fa-solid.fa-heart.add-to-favourite").on('click', (e) => {
+                let bookId = e.currentTarget.dataset.bookId;
+
+                // Call ajax and once it is succesful, check cart-count and add that number there
+                $.ajax({
+                    url: "{{ route('user.addfavourite') }}",
+                    method: 'POST',
+                    headers: {
+                        Accept: "application/json",
+                        Authorization: "{{ session('userToken') }}"
+                    },
+                    data: {
+                        book_id: bookId
+                    },
+                    success: (res) => {
+                        if (res.status === 'success') {
+                            toast('success', res.message);
+                            $("small.favourite-count").html(res.payload.favourite_count);
+                            $("small.favourite-count").css('display', 'flex');
+                        }
+                    },
+                    error: (jqXHR, exception) => {
+                        var errorMessage = "";
+
+                        if (jqXHR.status === 0) {
+                            errorMessage =
+                                "Not connect.\n Verify Network.";
+                        } else if (jqXHR.status == 404) {
+                            errorMessage =
+                                "Requested page not found. [404]";
+                        } else if (jqXHR.status == 409) {
+                            errorMessage = jqXHR.responseJSON.message;
+                        } else if (jqXHR.status == 500) {
+                            errorMessage =
+                                "Internal Server Error [500].";
+                        } else if (exception === "parsererror") {
+                            errorMessage =
+                                "Requested JSON parse failed.";
+                        } else if (exception === "timeout") {
+                            errorMessage = "Time out error.";
+                        } else if (exception === "abort") {
+                            errorMessage = "Ajax request aborted.";
+                        } else {
+                            let html = ''
+                            Object.values(jqXHR.responseJSON.errors).forEach((err) => {
+                                err.forEach((e) => {
+                                    html += `${e} <hr />`;
+                                });
+                            });
+                            Swal.fire({
+                                title: 'Error!',
+                                html: html,
+                                icon: 'error',
+                                animation: true,
+                                showConfirmButton: true,
+                            })
+                            return;
+                        }
+                        toast("error", errorMessage);
+                    }
+                })
             })
         });
     </script>
